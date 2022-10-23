31ad988ab55e25f4a6bb2e7ee8dbf654a67578e3
Bereket Terefe <berelliant11@gmail.com>
2022-10-23 13:51:35 +0300

    [init] - initialized from minifig-eb - added camera funcitonality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Footer } from "./components/Footer";
+
+export const AppContext = React.createContext();
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App light`}>      
+      <NavBar />
+      <Banner />
+      {/* <Footer /> */}
     </div>
   );
 }
